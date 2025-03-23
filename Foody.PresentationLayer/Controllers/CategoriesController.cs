9f841cea29b6578087fc818367cbdf360af44ccb
ozkzrl <ozkan.zorlu@gmail.com>
2025-03-23 22:20:54 +0300

    CategoryCreate was created.

--- Foody.PresentationLayer/Controllers/CategoriesController.cs
+++ Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
 
 using System.Security.Cryptography.X509Certificates;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 public class CategoriesController : Controller
 {
@@ -8,12 +10,31 @@ public class CategoriesController : Controller
 
     public CategoriesController(ICategoryService categoryService)
     {
-        _categoryService=categoryService;
+        _categoryService = categoryService;
 
     }
+
     public IActionResult CategoryList()
     {
-        var values=_categoryService.TGetAll();
+        var values = _categoryService.TGetAll();
         return View(values);
+    }
+
+    [HttpGet]
+    public IActionResult CreateCategory()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateCategory(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            // Kategori oluşturma işlemi
+            _categoryService.TAdd(category);
+            return RedirectToAction("CreateCategory"); // Başka bir sayfaya yönlendirebilirsiniz.
+        }
+        return View(category); // Eğer model geçerli değilse tekrar aynı sayfayı render et.
     }
 }
