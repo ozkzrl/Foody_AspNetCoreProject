926a477d0f22909eab34da36776bd9b0247a0616
ozkzrl <ozkan.zorlu@gmail.com>
2025-03-29 00:53:26 +0300

    Delete and uodate category were created.

--- Foody.PresentationLayer/Controllers/CategoriesController.cs
+++ Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -37,4 +37,34 @@ public class CategoriesController : Controller
         }
         return View(category); // Eğer model geçerli değilse tekrar aynı sayfayı render et.
     }
+
+    public IActionResult DeleteCategory(int id)
+    {
+        _categoryService.TDelete(id);
+        return RedirectToAction("CategoryList");
+    }
+
+    [HttpGet]
+    public IActionResult UpdateCategory(int id)
+    {
+        var value = _categoryService.TGetById(id);
+        return View(value);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateCategory(Category category)
+    {
+
+        if (ModelState.IsValid)
+        {
+            var existingCategory = _categoryService.TGetById(category.CategoryId); // ID'ye göre kaydı getir
+            if (existingCategory != null)
+            {
+                existingCategory.CategoryName = category.CategoryName; // Kategori adını güncelle
+                _categoryService.TUpdate(existingCategory); // Güncelleme işlemini gerçekleştir
+            }
+            return RedirectToAction("CategoryList");
+        }
+        return View(category); // Model geçersizse tekrar formu göster
+    }
 }
